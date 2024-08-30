e07bb3dfef0a134be7686228c996378d401fd610
anasyakubu <yakubuanas04@gmail.com>
2024-08-30 17:21:30 +0100

    BestPrompt complete

--- src/sections/BestPrompt/BestPrompt.tsx
+++ src/sections/BestPrompt/BestPrompt.tsx
@@ -1,12 +1,13 @@
 // import React from 'react'
+import "./BestPrompt.scss";
 import BestPromptCard from "../../components/BestPromptCard";
 
 const BestPrompt = () => {
   return (
     <div className="BestPrompt">
-      <div className="p-20">
+      <div className="p-10">
         <div className="">
-          <div className="grid gap-6 lg:grid-cols-3">
+          <div className="grid gap-3 lg:grid-cols-3 ">
             <BestPromptCard />
             <BestPromptCard />
             <BestPromptCard />
