2df560c7de8c95aace39cb6c1073f8decb34a50d
anasyakubu <yakubuanas04@gmail.com>
2024-08-30 18:26:01 +0100

    Add some sections

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,13 +2,31 @@
 import BestPrompt from "../../sections/BestPrompt/BestPrompt";
 import Hero from "../../sections/Hero/Hero";
 import RowSection from "../../sections/RowSection/RowSection";
+// import Image from "../../assets/site.png";
+import Summerise from "../../assets/summerise.jpg";
+import Meeting from "../../assets/meeting.jpg";
+import Code from "../../assets/code.jpg";
 
 const Home = () => {
   return (
     <div className="text-white">
       <Hero />
       <BestPrompt />
-      <RowSection />
+      <RowSection
+        bg="bg-yellow-600"
+        title="Writes, brainstorms, edits, and explores ideas with you"
+        image={Summerise}
+      />
+      <RowSection
+        bg="bg-blue-500"
+        title="Summarize meetings. Find new insights. Increase productivity."
+        image={Meeting}
+      />
+      <RowSection
+        bg="bg-indigo-500"
+        title="Generate and debug code. Automate repetitive tasks. Learn new APIs."
+        image={Code}
+      />
     </div>
   );
 };
--- src/sections/RowSection/RowSection.tsx
+++ src/sections/RowSection/RowSection.tsx
@@ -1,18 +1,22 @@
 // import React from 'react'
 import "./RowSection.scss";
-import Image from "../../assets/site.png";
+// import Image from "../../assets/site.png";
 
-const RowSection = () => {
+interface RowSectionProps {
+  title: string;
+  image: string;
+  bg: string;
+}
+
+const RowSection: React.FC<RowSectionProps> = ({ title, image, bg }) => {
   return (
     <div className="RowSection">
       <div className="p-24">
         <div className="text-center py-5">
-          <h2 className="text-5xl leading-snug max-w-3xl mx-auto">
-            Writes, brainstorms, edits, and explores ideas with you
-          </h2>
+          <h2 className="text-5xl leading-snug max-w-3xl mx-auto">{title}</h2>
         </div>
-        <div className="my-3 p-10 bg-yellow-600 rounded-xl shadow-lg">
-          <img className="rounded-lg shadow-lg" src={Image} alt="Image" />
+        <div className={`my-3 p-10 ${bg} rounded-xl shadow-lg`}>
+          <img className="rounded-lg shadow-lg" src={image} alt="Image" />
         </div>
       </div>
     </div>
