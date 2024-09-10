1183fded6962dc4be88a891634589a882871fd18
anasyakubu <yakubuanas04@gmail.com>
2024-09-10 16:22:05 +0100

    signin with google implemented

--- src/sections/LoginContent/LoginContent.tsx
+++ src/sections/LoginContent/LoginContent.tsx
@@ -1,11 +1,46 @@
 import React, { useState } from "react";
 import "./LoginContent.scss";
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../../firebase/firebase";
+import {
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+  UserCredential,
+} from "firebase/auth";
+import { auth, db } from "../../firebase/firebase";
+import { setDoc, doc } from "firebase/firestore";
+
 import { FcGoogle } from "react-icons/fc";
 import { toast } from "react-toastify";
 
 const LoginContent: React.FC = () => {
+  // Login with Google
+  const googleLogin = async () => {
+    const provider = new GoogleAuthProvider();
+    try {
+      const result: UserCredential = await signInWithPopup(auth, provider);
+      console.log(result);
+      const user = result.user;
+      if (user) {
+        await setDoc(doc(db, "Users", user.uid), {
+          email: user.email,
+          firstName: user.displayName,
+          photo: user.photoURL,
+          lastName: "",
+        });
+        toast.success("User logged in successfully", {
+          position: "top-center",
+        });
+        window.location.href = "/profile";
+      }
+    } catch (error: any) {
+      console.error("Error signing in with Google:", error.message);
+      toast.error(error.message, {
+        position: "bottom-center",
+      });
+    }
+  };
+
+  // Login with email and password
   // Define state with types
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
@@ -79,14 +114,7 @@ const LoginContent: React.FC = () => {
             </p>
           </div>
           <div className="mt-5 flex justify-center text-center">
-            <button
-              type="button"
-              className="google-btn"
-              // onClick={googleSignup}
-              onClick={() => {
-                toast.success("Not Available..");
-              }}
-            >
+            <button type="button" className="google-btn" onClick={googleLogin}>
               <span className="py-1">
                 <FcGoogle size={15} />
               </span>
